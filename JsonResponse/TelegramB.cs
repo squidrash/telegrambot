2e4958b446cb34d92e89800a1dd3ce5c3d4497d5
nikitarasevskij <squidrash777@gmail.com>
2020-10-22 15:23:50 +0300

    init

---
+++ JsonResponse/TelegramB.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace JsonResponse
+{
+    public class TelegramB
+    {
+        public TelegramB()
+        {
+            var result = new Result();
+        }
+        public bool ok { get; set; }
+        public Result[] result { get; set; }
+    }
+    public class Result
+    {
+        public Result()
+        {
+            Message message = new Message();
+        }
+        public int update_id { get; set; }
+        public Message message { get; set; }
+    }
+    public class Message
+    {
+        public Message()
+        {
+            From from = new From();
+            Chat chat = new Chat();
+        }
+        public int message_id { get; set; }
+        public From from { get; set; }
+        public Chat chat { get; set; }
+        public long date { get; set; }
+        public string text { get; set; }
+
+    }
+    public class From
+    {
+        public long id { get; set; }
+        public bool is_bot { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string username { get; set; }
+        public string language_code { get; set; }
+
+    }
+    public class Chat
+    {
+        public long id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string username { get; set; }
+        public string type { get; set; }
+
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using JsonResponse;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var client = new RestClient("https://api.telegram.org/bot1190000239:AAHgqTwRrLoHIAgbawXWtH-ZElKh4llIaLQ");
+
+                Console.WriteLine("1");
+                var request1 = new RestRequest("getMe", DataFormat.Json);
+                Console.WriteLine("2");
+                var request2 = new RestRequest("getUpdates", DataFormat.Json);
+                
+                var response = client.Get(request1);
+                var response2 = client.Get(request2);
+                
+                //var res = client.Post(req);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                var b = JsonConvert.DeserializeObject(response.Content);
+                
+                
+
+                Console.WriteLine(b);
+
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                var c = JsonConvert.DeserializeObject(response2.Content);
+                Console.WriteLine(c.GetType());
+
+
+                TelegramB telegram = JsonConvert.DeserializeObject<TelegramB>(response2.Content);
+                Console.WriteLine(telegram.result[telegram.result.Length-1].message.chat.id);
+                Console.WriteLine(telegram.result[telegram.result.Length - 1].message.text);
+
+                //Console.WriteLine(telegram.result[telegram.result.Length - 1]);
+                
+
+                //var id = telegram.result[telegram.result.Length - 1].message.chat.id;
+                //var text = telegram.result[telegram.result.Length - 1].message.text;
+                //var req = new RestRequest($"sendMessage?chat_id={id}&text=ты написал: {text}");
+
+                //client.Post(req);
+
+
+
+                //Console.WriteLine(c);
+                //Console.WriteLine(response2.Request);
+
+                Console.WriteLine("тест");
+                Console.ReadLine();
+            }
+        }
+    }
+}
