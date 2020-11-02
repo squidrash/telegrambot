a6d2478477ba23e44ac2cc2e95a5bd8d772d16e9
nikitarasevskij <squidrash777@gmail.com>
2020-11-02 14:18:08 +0300

    работающий эхо бот + начало погодного

--- JsonResponse/TelegramB.cs
+++ JsonResponse/TelegramB.cs
@@ -1,56 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JsonResponse
 {
     public class TelegramB
     {
-        public TelegramB()
-        {
-            var result = new Result();
-        }
-        public bool ok { get; set; }
-        public Result[] result { get; set; }
+        public bool Ok { get; set; }
+        public List<Result> Result { get; set; }
     }
     public class Result
     {
-        public Result()
-        {
-            Message message = new Message();
-        }
-        public int update_id { get; set; }
-        public Message message { get; set; }
+        public int Update_id { get; set; }
+        public Message Message { get; set; }
     }
     public class Message
     {
-        public Message()
-        {
-            From from = new From();
-            Chat chat = new Chat();
-        }
-        public int message_id { get; set; }
-        public From from { get; set; }
-        public Chat chat { get; set; }
-        public long date { get; set; }
-        public string text { get; set; }
+        public int Message_id { get; set; }
+        public From From { get; set; }
+        public Chat Chat { get; set; }
+        public long Date { get; set; }
+        public string Text { get; set; }
 
     }
     public class From
     {
-        public long id { get; set; }
-        public bool is_bot { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string username { get; set; }
-        public string language_code { get; set; }
+        public long Id { get; set; }
+        public bool Is_bot { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Username { get; set; }
+        public string Language_code { get; set; }
 
     }
     public class Chat
     {
-        public long id { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string username { get; set; }
-        public string type { get; set; }
+        public long Id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Username { get; set; }
+        public string Type { get; set; }
 
     }
 }
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Threading;
 using RestSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using JsonResponse;
+using System.Collections.Generic;
 
 namespace TelegramBot
 {
@@ -10,52 +12,119 @@ namespace TelegramBot
     {
         static void Main(string[] args)
         {
+            int offset = 0;
+            string city = null;
             while (true)
             {
-                var client = new RestClient("https://api.telegram.org/bot1190000239:AAHgqTwRrLoHIAgbawXWtH-ZElKh4llIaLQ");
+                var clientTelegram = new RestClient("https://api.telegram.org/bot1190000239:AAHgqTwRrLoHIAgbawXWtH-ZElKh4llIaLQ");
+                // подсказка api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+                var clientWeather = new RestClient("https://api.openweathermap.org/data/2.5");
+                
+                var getMe = new RestRequest("getMe", DataFormat.Json);
+                var getUpdates = new RestRequest("getUpdates", DataFormat.Json);
+                var getUpdatesOffset = new RestRequest($"getUpdates?offset={offset}", DataFormat.Json);
 
-                Console.WriteLine("1");
-                var request1 = new RestRequest("getMe", DataFormat.Json);
-                Console.WriteLine("2");
-                var request2 = new RestRequest("getUpdates", DataFormat.Json);
-                
-                var response = client.Get(request1);
-                var response2 = client.Get(request2);
-                
-                //var res = client.Post(req);
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                var b = JsonConvert.DeserializeObject(response.Content);
-                
-                
 
-                Console.WriteLine(b);
+                var response = clientTelegram.Get(getMe);
+                var response2 = clientTelegram.Get(getUpdates);
+                var response3 = clientTelegram.Get(getUpdatesOffset);
 
                 Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                var c = JsonConvert.DeserializeObject(response2.Content);
-                Console.WriteLine(c.GetType());
+                TelegramB telegram = JsonConvert.DeserializeObject<TelegramB>(response3.Content);
 
-
-                TelegramB telegram = JsonConvert.DeserializeObject<TelegramB>(response2.Content);
-                Console.WriteLine(telegram.result[telegram.result.Length-1].message.chat.id);
-                Console.WriteLine(telegram.result[telegram.result.Length - 1].message.text);
-
-                //Console.WriteLine(telegram.result[telegram.result.Length - 1]);
-                
-
-                //var id = telegram.result[telegram.result.Length - 1].message.chat.id;
-                //var text = telegram.result[telegram.result.Length - 1].message.text;
-                //var req = new RestRequest($"sendMessage?chat_id={id}&text=ты написал: {text}");
-
-                //client.Post(req);
-
-
-
-                //Console.WriteLine(c);
-                //Console.WriteLine(response2.Request);
-
-                Console.WriteLine("тест");
-                Console.ReadLine();
+                foreach (var r in telegram.Result)
+                {
+                    Console.WriteLine($"Update id: {r.Update_id}");
+                    Console.WriteLine($"Chat id: {r.Message.Chat.Id}");
+                    Console.WriteLine($"First name : {r.Message.From.First_name}");
+                    Console.WriteLine($"Last name: {r.Message.From.Last_name}");
+                    Console.WriteLine($"Text: {r.Message.Text}");
+                    var id = r.Message.Chat.Id;
+                    var text = r.Message.Text;
+                    var req = new RestRequest($"sendMessage?chat_id={id}&text=ты написал: {text}");
+                    clientTelegram.Post(req);
+                    city = text;
+                    //var APIKeyWeather = "a5c2b591223f038ff62ea42e92f43c74";
+                    //var getWeather = new RestRequest($"weather?q={city}&appid={APIKeyWeather}", DataFormat.Json);
+                    //var responseWeather = clientWeather.Get(getWeather);
+                    //OpenWeather openWeather = JsonConvert.DeserializeObject<OpenWeather>(responseWeather.Content);
+                    //Console.WriteLine(openWeather.MainW.Temp);
+                    
+                }
+                try
+                {
+                    //Console.WriteLine(telegram.Result[^1].Update_id);
+                    offset = telegram.Result[^1].Update_id + 1;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    offset = 0;
+                }
+                //Thread.Sleep(1000);
+                //Console.ReadLine();
             }
+        }
+        public class OpenWeather
+        {
+            public Coord Coord { get; set; }
+            public List<Weather> Weather { get; set; }
+            public string Base { get; set; }
+            [JsonProperty("main")]
+            public MainW MainW { get; set; }
+            public int Visibility { get; set; }
+            public Wind Wind { get; set; }
+            public Rain Rain { get; set; }
+            public Clouds Clouds { get; set; }
+            public int Dt { get; set; }
+            public Sys Sys { get; set; }
+            public int Timezone { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Cod { get; set; }
+        }
+        public class Coord
+        {
+            public double Lon { get; set; }
+            public double Lat { get; set; }
+        }
+        public class Weather
+        {
+            public int Id { get; set; }
+            public string Main { get; set; }
+            public string Description { get; set; }
+            public string Icon { get; set; }
+        }
+        public class MainW
+        {
+            public double Temp { get; set; }
+            public double Feels_like { get; set; }
+            public double Temp_min { get; set; }
+            public double Temp_max { get; set; }
+            public double Pressure { get; set; }
+            public double Humidity { get; set; }
+        }
+        public class Wind
+        {
+            public double Speed { get; set; }
+            public int Deg { get; set; }
+        }
+        public class Rain
+        {
+            [JsonProperty("1h")]
+            public double OneH { get; set; }
+        }
+        public class Clouds
+        {
+            public int All { get; set; }
+        }
+        public class Sys
+        {
+            public int Type { get; set; }
+            public int Id { get; set; }
+            public string Country { get; set; }
+            public int Sunrise { get; set; }
+            public int Sunset { get; set; }
         }
     }
 }
